eedebd79af9be8aca70806d570e7e0c971dfc38e
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-04 12:35:54 -0500

    Updating javascript for additional URLs

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
 
 //https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/
+//https://developer-acct.ticketmaster.com/user/login?destination=user
 
-const baseURL = "https://app.ticketmaster.com/discovery/v2/"
-const endpoint = "events.json?countryCode=US&"
-const apikey = "apikey=107CcXtgxDmxSxoIjOucSPDRBTMyB9Gn"
+//const apikey = "apikey=107CcXtgxDmxSxoIjOucSPDRBTMyB9Gn"
 
-const url = baseURL + endpoint + apikey
+const baseURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=107CcXtgxDmxSxoIjOucSPDRBTMyB9Gn&keyword="
 
+function getEvent(eventName) {
+const url = baseURL + eventName
+    //console.log(url)
 fetch(url)
-.then(res => res.json())
-.then(data => console.log(data))
+.then(res => {return res.json()})
+.then(data => {
+    returnEventRequest(data)
+    console.log(data)
+    })
+}
 
+function returnEventRequest(event) {
+    const eventDiv = document.querySelector(".event")
+    eventDiv.innerHTML = `
+    <h3>${event._embedded.events[0].name}</h3>
+    <h6>${event._embedded.events[0].url}</h6>
 
+    <h3>${event._embedded.events[1].name}</h3>
+    <h6>${event._embedded.events[1].url}</h6>
 
+    // <h3>${event._embedded.events[2].name}</h3>
+    // <h6>${event._embedded.events[2].url}</h6>
 
+    // <h3>${event._embedded.events[3].name}</h3>
+    // <h6>${event._embedded.events[3].url}</h6>
 
-// function getPlayer(playerName){
-    
+    // <h3>${event._embedded.events[4].name}</h3>
+    // <h6>${event._embedded.events[4].url}</h6>
 
-//     console.log(url)
+    // <h3>${event._embedded.events[5].name}</h3>
+    // <h6>${event._embedded.events[5].url}</h6>
 
+    // <h3>${event._embedded.events[6].name}</h3>
+    // <h6>${event._embedded.events[6].url}</h6>
 
-// fetch(url)
-// .then(res => {return res.json()})
-// .then((data) => {
-//     renderPlayerStats(data)
-// })
-// }
+    // <h3>${event._embedded.events[7].name}</h3>
+    // <h6>${event._embedded.events[7].url}</h6>
 
+    // <h3>${event._embedded.events[8].name}</h3>
+    // <h6>${event._embedded.events[8].url}</h6>
 
+    // <h3>${event._embedded.events[9].name}</h3>
+    // <h6>${event._embedded.events[9].url}</h6>
 
+    `    
+}
 
+function eventRequest(request) {
+    request.preventDefault()
+    const form = request.target;
+    //console.log(form)
+    const formData = new FormData(form)
+    const eventName = formData.get("eventName")
+    //console.log(eventName)
+    getEvent(eventName)
+}
+
+document.querySelector("form").addEventListener("submit", eventRequest)
+getEvent("Buffalo Sabres")
