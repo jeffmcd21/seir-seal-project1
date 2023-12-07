0814ebc22de08366f1f4e30767b81252aca0ca91
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-06 19:33:13 -0500

    Status check

--- script.js
+++ script.js
@@ -21,34 +21,65 @@ function returnEventRequest(event) {
     const eventDiv = document.querySelector(".event")
     eventDiv.innerHTML = `
     <h3>${event._embedded.events[0].name}</h3>
-    <a href="${event._embedded.events[0].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[0].images[1].url}> 
+    <a href="${event._embedded.events[0].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[0].dates.start.localDate}">Event Date: ${event._embedded.events[0].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[0].dates.start.localTime}">Event Time: ${event._embedded.events[0].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[1].name}</h3>
-    <a href="${event._embedded.events[1].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[1].images[1].url}> 
+    <a href="${event._embedded.events[1].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[1].dates.start.localDate}">Event Date: ${event._embedded.events[1].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[1].dates.start.localTime}">Event Time: ${event._embedded.events[1].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[2].name}</h3>
-    <a href="${event._embedded.events[2].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[2].images[1].url}> 
+    <a href="${event._embedded.events[2].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[2].dates.start.localDate}">Event Date: ${event._embedded.events[2].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[2].dates.start.localTime}">Event Time: ${event._embedded.events[2].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[3].name}</h3>
-    <a href="${event._embedded.events[3].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[3].images[1].url}> 
+    <a href="${event._embedded.events[3].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[3].dates.start.localDate}">Event Date: ${event._embedded.events[3].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[3].dates.start.localTime}">Event Time: ${event._embedded.events[3].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[4].name}</h3>
-    <a href="${event._embedded.events[4].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[4].images[1].url}> 
+    <a href="${event._embedded.events[4].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[4].dates.start.localDate}">Event Date: ${event._embedded.events[4].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[4].dates.start.localTime}">Event Time: ${event._embedded.events[4].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[5].name}</h3>
-    <a href="${event._embedded.events[5].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[5].images[1].url}> 
+    <a href="${event._embedded.events[5].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[5].dates.start.localDate}">Event Date: ${event._embedded.events[5].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[5].dates.start.localTime}">Event Time: ${event._embedded.events[5].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[6].name}</h3>
-    <a href="${event._embedded.events[6].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[6].images[1].url}> 
+    <a href="${event._embedded.events[6].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[6].dates.start.localDate}">Event Date: ${event._embedded.events[6].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[6].dates.start.localTime}">Event Time: ${event._embedded.events[6].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[7].name}</h3>
-    <a href="${event._embedded.events[7].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[7].images[1].url}> 
+    <a href="${event._embedded.events[7].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[7].dates.start.localDate}">Event Date: ${event._embedded.events[7].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[7].dates.start.localTime}">Event Time: ${event._embedded.events[7].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[8].name}</h3>
-    <a href="${event._embedded.events[8].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[8].images[1].url}> 
+    <a href="${event._embedded.events[8].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[8].dates.start.localDate}">Event Date: ${event._embedded.events[8].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[8].dates.start.localTime}">Event Time: ${event._embedded.events[8].dates.start.localTime}</a>
 
     <h3>${event._embedded.events[9].name}</h3>
-    <a href="${event._embedded.events[9].url}">Click here for seating chart, pricing, and tickets</a>"
+    <img style="width: 200px;" + src=${event._embedded.events[9].images[1].url}> 
+    <a href="${event._embedded.events[9].url}">Click here for seating chart, pricing, and tickets</a>", 
+    <a href="${event._embedded.events[9].dates.start.localDate}">Event Date: ${event._embedded.events[9].dates.start.localDate}</a>, 
+    <a href="${event._embedded.events[9].dates.start.localTime}">Event Time: ${event._embedded.events[9].dates.start.localTime}</a>
+    
 
     `    
 }
